9c9e9ed3e090c70400266dfe3368ab75b295497e
Binh An Pham <anbinhpham110799@gmail.com>
2022-09-04 13:10:27 -0400

    getNFTs, searchNFTs, getNFTsForContract, getNFTTransfers

--- moralis_API_client.py
+++ moralis_API_client.py
@@ -1,43 +1,50 @@
 import requests
 from dotenv import load_dotenv
 import os
+import asyncio
 
-load_dotenv()
-moralis_API_key = os.getenv("MORALIS_API_key")
+class MoralisClient:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        load_dotenv()
+        self.headers = {
+            'accept': 'application/json','X-API-Key': os.getenv("MORALIS_API_key"),
+        }
+    
+    def handle_request(self, url):
+        return requests.get(
+            url,
+            headers=self.headers
+        )
 
-def get_call(url, headers):
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.HTTPError as errh:
-        print ("Http Error:", errh)
-    except requests.exceptions.ConnectionError as errc:
-        print ("Error Connecting:", errc)
-    except requests.exceptions.Timeout as errt:
-        print ("Timeout Error:", errt)
-    except requests.exceptions.RequestException as err:
-        print ("OOps: Something Else", err)
+    async def handle_response(self, response):
+        try:
+            return response.json()
+        except requests.exceptions.HTTPError as errh:
+            print ("Http Error:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print ("Error Connecting:", errc)
+        except requests.exceptions.Timeout as errt:
+            print ("Timeout Error:", errt)
+        except requests.exceptions.RequestException as err:
+            print ("OOps: Something Else", err)
 
-def moralis_headers():
-    return {"accept": "application/json","X-API-Key": moralis_API_key}
+    async def searchNFTs(self, q, filter, chain="Eth", format="decimal", limit=100):
+        url = f'https://deep-index.moralis.io/api/v2/nft/search?chain={chain}&format={format}&q={q}&filter={filter}&limit={limit}'
+        response = self.handle_request(url)
+        return await self.handle_response(response)
 
-def searchNFTs(q, filter, chain="Eth", format="decimal", limit=100):
-    url = f'https://deep-index.moralis.io/api/v2/nft/search?chain={chain}&format={format}&q={q}&filter={filter}&limit={limit}'
-    headers = moralis_headers()
-    return get_call(url, headers)
+    async def getNFTs(self, address, chain="Eth", format="decimal"):
+        url = f'https://deep-index.moralis.io/api/v2/{address}/nft?chain={chain}&format={format}'
+        response = self.handle_request(url)
+        return await self.handle_response(response)
 
-def getNFTs(address, chain="Eth", format="decimal"):
-    url = f'https://deep-index.moralis.io/api/v2/{address}/nft?chain={chain}&format={format}'
-    headers = moralis_headers()
-    return get_call(url, headers)
+    async def getNFTsForContract(self, address, token_address="", chain="Eth", format="decimal", cursor="", limit=100):
+        url = f'https://deep-index.moralis.io/api/v2/{address}/nft/{token_address}chain={chain}&format={format}'
+        response = self.handle_request(url)
+        return await self.handle_response(response)
 
-def getNFTsForContract(address, token_address="", chain="Eth", format="decimal", cursor="", limit=100):
-    url = f'https://deep-index.moralis.io/api/v2/{address}/nft/{token_address}chain={chain}&format={format}'
-    headers = moralis_headers()
-    return get_call(url, headers)
-
-def getNFTTransfers(address, chain="Eth", direction="both", format="decimal", cursor="", limit=100):
-    url = f'https://deep-index.moralis.io/api/v2/{address}/nft/transfers?chain={chain}&format={format}'
-    headers = moralis_headers()
-    return get_call(url, headers)
+    async def getNFTTransfers(self, address, chain="Eth", direction="both", format="decimal", cursor="", limit=100):
+        url = f'https://deep-index.moralis.io/api/v2/{address}/nft/transfers?chain={chain}&format={format}'
+        response = self.handle_request(url)
+        return await self.handle_response(response)
